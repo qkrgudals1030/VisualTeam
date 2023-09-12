8a7256cc0fb130337e29747d01627d6f23ff60ce
qkrgudals1030 <50895124+qkrgudals1030@users.noreply.github.com>
2023-09-12 22:10:35 +0900

    Add files via upload

---
+++ CPP/PeDoc.cpp
@@ -0,0 +1,136 @@
+﻿
+// PeDoc.cpp: CPeDoc 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Pe.h"
+#endif
+
+#include "PeDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CPeDoc
+
+IMPLEMENT_DYNCREATE(CPeDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CPeDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CPeDoc 생성/소멸
+
+CPeDoc::CPeDoc() noexcept
+{
+	// TODO: 여기에 일회성 생성 코드를 추가합니다.
+
+}
+
+CPeDoc::~CPeDoc()
+{
+}
+
+BOOL CPeDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 여기에 재초기화 코드를 추가합니다.
+	// SDI 문서는 이 문서를 다시 사용합니다.
+	INT_PTR n = m_oa.GetCount();
+	for (INT_PTR i = 0; i < n; i++) {
+		delete m_oa[i];
+	}
+	m_oa.RemoveAll();
+	return TRUE;
+}
+
+
+
+
+// CPeDoc serialization
+
+void CPeDoc::Serialize(CArchive& ar)
+{
+	m_oa.Serialize(ar); // CObArray m_oa
+}
+
+#ifdef SHARED_HANDLERS
+
+// 축소판 그림을 지원합니다.
+void CPeDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 문서의 데이터를 그리려면 이 코드를 수정하십시오.
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 검색 처리기를 지원합니다.
+void CPeDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 문서의 데이터에서 검색 콘텐츠를 설정합니다.
+	// 콘텐츠 부분은 ";"로 구분되어야 합니다.
+
+	// 예: strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CPeDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CPeDoc 진단
+
+#ifdef _DEBUG
+void CPeDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CPeDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CPeDoc 명령
+
---
+++ CPP/PeView.cpp
@@ -0,0 +1,171 @@
+﻿
+// PeView.cpp: CPeView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Pe.h"
+#endif
+
+#include "PeDoc.h"
+#include "PeView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CPeView
+
+IMPLEMENT_DYNCREATE(CPeView, CView)
+
+BEGIN_MESSAGE_MAP(CPeView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_WM_MOUSEMOVE()
+	ON_COMMAND(ID_SELECT_COLOR, &CPeView::OnSelectColor)
+	ON_COMMAND(ID_SIZE_1, &CPeView::OnSize1)
+	ON_COMMAND(ID_SIZE_4, &CPeView::OnSize4)
+	ON_COMMAND(ID_SIZE_8, &CPeView::OnSize8)
+	ON_COMMAND(ID_SIZE_16, &CPeView::OnSize16)
+	ON_COMMAND(ID_SIZE_32, &CPeView::OnSize32)
+END_MESSAGE_MAP()
+
+// CPeView 생성/소멸
+
+CPeView::CPeView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CPeView::~CPeView()
+{
+}
+
+BOOL CPeView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CPeView 그리기
+#include "CLine.h"
+void CPeView::OnDraw(CDC* pDC)
+{
+	CPeDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	int n = pDoc->m_oa.GetSize(); //크기를 나타낸다
+	for (int i = 0; i < n; i++) {
+		CLine* pLine =( CLine*) pDoc->m_oa[i];
+		pLine->Draw(pDC);
+	}
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CPeView 인쇄
+
+BOOL CPeView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CPeView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CPeView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CPeView 진단
+
+#ifdef _DEBUG
+void CPeView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CPeView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CPeDoc* CPeView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CPeDoc)));
+	return (CPeDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CPeView 메시지 처리기
+COLORREF Col;
+int Size;
+
+#include"CLine.h"
+void CPeView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	if (nFlags == MK_LBUTTON) {
+	CPeDoc* pDoc = GetDocument();
+	CLine* pLine = new CLine(opnt, point, Size, Col);
+	pDoc->m_oa.Add(pLine);
+	Invalidate(FALSE);  //화면을 새로그려준다
+}
+	opnt = point;
+	CView::OnMouseMove(nFlags, point);
+}
+
+
+void CPeView::OnSelectColor()
+{
+	CColorDialog dlg;
+	if (dlg.DoModal() == IDOK);
+	Col = dlg.GetColor();
+}
+
+
+void CPeView::OnSize1()
+{
+	Size = 1;
+
+}
+
+
+void CPeView::OnSize4()
+{
+	Size = 4;
+}
+
+
+void CPeView::OnSize8()
+{
+	Size = 8;
+}
+
+
+void CPeView::OnSize16()
+{
+	Size = 16;
+}
+
+
+void CPeView::OnSize32()
+{
+	Size = 32;
+}
